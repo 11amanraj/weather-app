29ad34274f28b99e5918f0f1fee4a0dcf90152cd
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-07-30 23:07:33 +0530

    Added type definition for weather object

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 import Dashboard from '@/components/Dashboard'
 import Search from '@/components/Search'
 import Sidebar from '@/components/Sidebar'
-import Image from 'next/image'
+
+interface weather {
+  city: string,
+  lat: number,
+  long: number,
+  wind: {
+    speed: number,
+    deg: number,
+    gust: number
+  }, 
+  main: {
+    temp: number,
+    feelsLike: number,
+    minTemp: number,
+    maxTemp: number,
+    humidity: number,
+    pressure: number
+  },
+  weather: [
+    {
+      main: string,
+      description: string,
+      icon: string
+    }
+  ]
+}
 
 export default function Home() {
   return (
